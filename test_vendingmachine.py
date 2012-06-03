73f4b410786f3afb30b87c8994727bfe5c8b7bba
heavenshell <heavenshell.jp@gmail.com>
2012-06-03 23:56:41 +0900

    Change to use prameterized

--- test_vendingmachine.py
+++ test_vendingmachine.py
@@ -1,26 +1,23 @@
 # -*- coding: utf-8 -*-
 from unittest import TestCase
+from functools import wraps
 
 
-def data_provider(fn_data_provider):
-    """Data provider decorator, allows another callable to provide the data for the test"""
-    def test_decorator(fn):
-        def repl(self, *args):
-            for i in fn_data_provider():
-                try:
-                    fn(self, *i)
-                except AssertionError:
-                    print "Assertion error caught with data set ", i
-                    raise
-        return repl
-    return test_decorator
+def paramterized(**kwargs):
+    def _paramterized_test(func):
+        @wraps(func)
+        def __paramterized_test(*args):
+            func(args[0], kwargs)
+            return func
+        return __paramterized_test
+    return _paramterized_test
+
 
 class Drink(object):
 
     def __init__(self, name, price):
         self.name = name
         self.price = price
-
 
 
 class VendorMachine(object):
@@ -33,6 +30,7 @@ class VendorMachine(object):
 
         for i in range(self.default_amount):
             self.store(Drink('cola', 120))
+
 
     def add_coin(self, coin):
         if coin not in self.available_money:
@@ -54,7 +52,9 @@ class VendorMachine(object):
         if drink.name in self.products:
             amount += self.products[drink.name]['amount']
 
-        self.products.update({drink.name: {'price': drink.price, 'amount': amount + 1}})
+        self.products.update({
+            drink.name: {'price': drink.price, 'amount': amount + 1}}
+        )
 
     def get_price(self, name):
         return self.products[name]['price']
@@ -63,30 +63,26 @@ class VendorMachine(object):
         return self.products[name]['amount']
 
 
-
-
 class TDDBCTest(TestCase):
     def setUp(self):
         self.vm = VendorMachine()
         self.money = self.vm.available_money
 
-   # money = lambda: (10, 50, 100, 500, 1000)
-
-   # @data_provider(money)
-   # def test_foobar(self, money):
-   #     """ 硬貨、紙幣を投入できる """
-   #     self.assertEqual(self.vm.add_coin(money), 10)
-
-    def test_add_money(self):
+    @paramterized(param=10, expected=None)
+    @paramterized(param=50, expected=None)
+    @paramterized(param=100, expected=None)
+    @paramterized(param=500, expected=None)
+    @paramterized(param=1000, expected=None)
+    def test_paramterize1d(self, params):
         """ 硬貨、紙幣を投入できる """
-        for m in self.money:
-            self.assertEqual(self.vm.add_coin(m), None)
+        self.assertEqual(self.vm.add_coin(params['param']), params['expected'])
 
-    def test_add_error(self):
+    @paramterized(param=1, expected=1)
+    @paramterized(param=5, expected=5)
+    @paramterized(param=10000, expected=10000)
+    def test_paramterized_error(self, params):
         """ １円玉５円玉１万円を投入すると金額が返ってくる """
-        fetal_money = (1, 5, 10000)
-        for m in fetal_money:
-            self.assertEqual(self.vm.add_coin(m), m)
+        self.assertEqual(self.vm.add_coin(params['param']), params['expected'])
 
     def test_show_total_price(self):
         """ 投入金額の総計を取得できる """
