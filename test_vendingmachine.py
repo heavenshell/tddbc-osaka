998826e224f25455ea7e999347ade6ac58ec4e5e
heavenshell <heavenshell.jp@gmail.com>
2012-06-03 13:59:18 +0900

    Step 1

--- test_vendingmachine.py
+++ test_vendingmachine.py
@@ -1,12 +1,31 @@
 # -*- coding: utf-8 -*-
 from unittest import TestCase
 
+
+def data_provider(fn_data_provider):
+    """Data provider decorator, allows another callable to provide the data for the test"""
+    def test_decorator(fn):
+        def repl(self, *args):
+            for i in fn_data_provider():
+                try:
+                    fn(self, *i)
+                except AssertionError:
+                    print "Assertion error caught with data set ", i
+                    raise
+        return repl
+    return test_decorator
+
+
 class VendorMachine(object):
     def __init__(self):
         self.money = 0
         pass
 
     def add_coin(self, coin):
+        if coin == 30:
+            return coin
+
+
         self.money += coin
 
     def show_price(self):
@@ -17,13 +36,32 @@ class TDDBCTest(TestCase):
     def setUp(self):
         self.vm = VendorMachine()
 
-    def test_add_10(self):
-        """ 10 円玉を投入できる """
-        self.assertEqual(self.vm.add_coin(10), None)
 
-    #def test_add_error(self):
-    #    """ ３０円玉を投入すると金額が返ってくる """
-    #    self.assertEqual(self.vm.add_coin(30), 30)
+   # money = lambda: (10, 50, 100, 500, 1000)
+    money = (10, 50, 100, 500, 1000)
+
+   # @data_provider(money)
+   # def test_foobar(self, money):
+   #     """ 硬貨、紙幣を投入できる """
+   #     self.assertEqual(self.vm.add_coin(money), 10)
+
+    def test_add_(self):
+        """ 硬貨、紙幣を投入できる """
+        for m in self.money:
+            self.assertEqual(self.vm.add_coin(m), None)
+
+
+
+
+
+
+
+    def test_add_error(self):
+        """ ３０円玉を投入すると金額が返ってくる """
+        self.assertEqual(self.vm.add_coin(30), 30)
+
+
+
 
     def test_add(self):
         """ 投入金額の総計を取得できる """
